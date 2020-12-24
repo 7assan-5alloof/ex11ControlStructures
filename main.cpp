3c7f024207f604d9e582fa3625284a977a03feae
Hassan Khalloof <hassankhalloof20@gmail.com>
2020-12-25 00:18:19 +0400

    Complete project (#2)

--- main.cpp
+++ main.cpp
@@ -4,6 +4,29 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Press Ctrl-C to stop" << endl;
+
+    while (true)
+    {
+        int year;
+        bool typeOfYear;
+        cout << "Enter a year: ";
+        cin >> year;
+        cout << "Enter 0 for Gregorian, 1 for Hijri: ";
+        cin >> typeOfYear;
+
+        int peerYear;
+        if (typeOfYear)
+        {
+            peerYear = 0.97023 * year + 621.57;
+            cout << year << " Hijri matches " << peerYear << " A.D" << endl;
+        }
+        else
+        {
+            peerYear = (year - 621.57) / 0.97023;
+            cout << year << " A.D matches " << peerYear << " Hijri" << endl;
+        }
+    }
+
     return 0;
 }
